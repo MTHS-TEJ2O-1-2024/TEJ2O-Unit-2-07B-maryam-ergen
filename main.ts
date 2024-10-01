3726c5cca5af4571637f8e0556b5db0d39a3d303
maryam-ergen <maryam.ergen@mths.ca>
2024-10-01 11:18:32 -0400

    made the cookie clicker

--- main.ts
+++ main.ts
@@ -1,8 +1,19 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
- * This program ...
+ * Created by: Maryam Ergen
+ * Created on: Sep 2024
+ * This program plays cookie clicker
 */
 
-basic.showString('Hello, World!')
+basic.clearScreen()
+pins.digitalWritePin(DigitalPin.P16, 0)
+basic.showIcon(IconNames.Happy)
+
+let cookieClickNumber: number
+cookieClickNumber = 0
+
+input.onButtonPressed(Button.A, function () {
+    cookieClickNumber = cookieClickNumber + 1
+    pause(2)
+    basic.showNumber(cookieClickNumber)
+})
